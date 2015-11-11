4dddb6afc8d84bf8a076dfadb4e673509007ba86
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-11-11 21:56:48 +0000

    Servo using pulse width

--- Servo.ino
+++ Servo.ino
@@ -1,17 +1,33 @@
 #include <Servo.h>
 
 Servo servo;
-int t0;
+unsigned long t0;
+int pulse;
+char dir;
 
 void setup() {
   servo.attach(6);
   t0 = millis();
+  pulse = 0;
+  dir = 1;
 }
 
 void loop() {
-  int dt = (millis() - t0) * 0;
-  if (dt < 0) dt = 0;
-  int phase = dt % 360;
-  int angle = phase <= 180 ? phase : 360 - phase;
-  servo.write(angle);
+  // use writeMicroseconds, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH
+  int dt = millis() - t0;
+  t0 += dt;
+  if (dir == 1) {
+    pulse += dt;
+    if (pulse > MAX_PULSE_WIDTH) {
+      pulse = MAX_PULSE_WIDTH;
+      dir = 0;
+    }
+  } else {
+    pulse -= dt;
+    if (pulse < MIN_PULSE_WIDTH) {
+      pulse = MIN_PULSE_WIDTH;
+      dir = 1;
+    }
+  };
+  servo.writeMicroseconds(pulse);
 }
