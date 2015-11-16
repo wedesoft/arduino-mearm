28fddbaa398a28056108a2e35732c4787b36b5a1
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-11-16 07:40:45 +0000

    Wait at middle position

--- Servo.ino
+++ Servo.ino
@@ -3,30 +3,44 @@
 Servo servo;
 unsigned long t0;
 int pulse;
+int wait;
 char dir;
 
 void setup() {
   servo.attach(6);
   t0 = millis();
-  pulse = 0;
-  dir = 1;
+  pulse = DEFAULT_PULSE_WIDTH;
+  dir = 0;
+  wait = 5000;
 }
 
 void loop() {
   // use writeMicroseconds, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH
   int dt = millis() - t0;
   t0 += dt;
-  if (dir == 1) {
+  if (dir == 0) {
+    wait -= dt;
+    if (wait < 0) {
+      wait = 5000;
+      dir = 1;
+    };
+  } else if (dir == 1) {
     pulse += dt;
     if (pulse > MAX_PULSE_WIDTH) {
       pulse = MAX_PULSE_WIDTH;
-      dir = 0;
+      dir = 2;
     }
-  } else {
+  } else if (dir == 2) {
     pulse -= dt;
     if (pulse < MIN_PULSE_WIDTH) {
       pulse = MIN_PULSE_WIDTH;
-      dir = 1;
+      dir = 3;
+    }
+  } else {
+    pulse += dt;
+    if (pulse > DEFAULT_PULSE_WIDTH) {
+      pulse = DEFAULT_PULSE_WIDTH;
+      dir = 0;
     }
   };
   servo.writeMicroseconds(pulse);
