9db31e0a724e9d8624e1956d5db0830f624dddf6
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-11-25 21:59:51 +0000

    Adjusted some limits

--- arduino/mearm.ino
+++ arduino/mearm.ino
@@ -4,10 +4,10 @@
 
 // BASE, SHOULDER, ELBOW, GRIPPER
 const int SERVOPIN[] = {11, 9, 10, 6};
-const int OFFSET[] = {1380, 1500, 1589, 1500};
+const int OFFSET[] = {1380, 1500, 1589, 1327};
 const int MIN[] = {544, 544, 544, 544};// must not be below 544
-const int MAX[] = {2400, 2400, 1950, 2400};// must not be above 2400
-const float RESOLUTION[] = {11.12222, 11.12222, 9.8888, 11.12222};
+const int MAX[] = {2400, 2400, 1950, 2022};// must not be above 2400
+const float RESOLUTION[] = {11.12222, 11.12222, 9.8888, 15.44444};
 
 
 class ServoCurve: public Curve
