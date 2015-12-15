aeaede186791bf014727dc3d2590a80d297e9898
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-12-15 22:55:09 +0000

    12 teach points

--- test-suite.cc
+++ test-suite.cc
@@ -498,7 +498,7 @@ TEST_F(ControllerTest, OnlyAlphabeticTeachPoints) {
 TEST_F(ControllerTest, WrongTeachPointKeyStopsDrives) {
   m_controller.targetAngle(BASE, 0);
   m_controller.parseChar('@');
-  m_controller.parseChar('u');
+  m_controller.parseChar('@');
   EXPECT_EQ(45, m_controller.curve(BASE).target());
 }
 
